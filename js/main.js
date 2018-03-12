6717f386de560153f829aed91a42251058dbbc35
Mahmoud Ahmed Ussien <32422962+Mahmoud126@users.noreply.github.com>
2018-03-12 15:54:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@ createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
   const image = document.createElement('img');
-  image.setAttribute("alt","restaurant Reviews");
+  image.setAttribute("alt","People eating in a restaurant");
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
